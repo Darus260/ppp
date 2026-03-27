486e0cd8443d1b68c5adff0b562e5dfb647effa5
Darus260 <91655276+Darus260@users.noreply.github.com>
2026-03-27 20:14:54 +0700

    Change BASE_URL to a placeholder value

--- bb1.py
+++ bb1.py
@@ -8,7 +8,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 # --- KONFIGURASI ---
 FILE_INPUT = "NWSHP_codes_265277_299999.txt"  
 FILE_OUTPUT = "voucher_valid_final.txt"
-BASE_URL = "https://myskill.id/payment/review-cv-ai/695b6ba96qgWXGrJQigK?coupon="
+BASE_URL = "#"
 
 # TENTUKAN MODE DI SINI:
 # False = Buka browser (Untuk login pertama kali atau melihat prosesnya)
